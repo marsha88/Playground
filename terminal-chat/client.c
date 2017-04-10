d08806f25bf9f2163745573b3b44cda40c51f2b9
marsha88 <marsha88@purdue.edu>
2017-04-09 22:02:30 -0400

    Client socket setup

--- terminal-chat/client.c
+++ terminal-chat/client.c
@@ -4,10 +4,20 @@
 #include <netinet/in.h>
 #include <string.h>
 
+#define PORT 3000
 
 int main(int argc, char *argv[]){
 
+	int main_socket = socket(AF_INET, SOCK_STREAM, 0);	
+	if( main_socket < 0 ){
+		fprintf(stderr, "Error: main_socket < 0");
+	}
+	
+	struct sockaddr_in serverAddr;
+	memset( &serverAddr, 0, sizeof(serverAddr) );
 
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons( PORT ); 
 
 
 	return 0;
