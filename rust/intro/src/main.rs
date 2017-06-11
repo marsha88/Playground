ff84c701ed79c7fb0aa1b09ac326231109f4c4de
marsha88 <marsha88@purdue.edu>
2017-06-11 16:31:17 -0400

    if let statements

--- rust/intro/src/main.rs
+++ rust/intro/src/main.rs
@@ -1,4 +1,4 @@
-
+#[warn(dead_code)]
 fn add_nums(x:i32, y:i32)->i32{
 	x + y
 }
@@ -20,8 +20,38 @@ fn slice(word: &str) -> &str{
 	&word[..5]
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+enum coin{
+	Penny,
+	Nickel,
+	Dime,
+	Quarter(i32),
+	Half_Dollar		
+}
+
+fn silver_mining(coins:Vec<coin>)->u32{
+	/* cycle through coins and determine if they are made of silver*/
+	/* return the number of coins that are silver */
+	let mut count = 0;
+	for x in 0..coins.len(){
+		if let coin::Quarter(year) = coins[x]{
+			if year <= 1965{
+				count += 1;
+			}				
+		}						
+
+	}
+	count 
+}
+
 fn main() {
-	let a:&str = "Hello, world";
-	let hello:&str = slice(a);
-	println!("{:?}",str);
+	//Find the silver quarters
+	let coins = vec![coin::Quarter(1965), coin::Quarter(1965),coin::Quarter(1999), coin::Penny, coin::Nickel, coin::Quarter(1960)];
+	println!("This set of coins has {} silver coins",silver_mining(coins));
+		
 }
--- rust/intro/target/debug/deps/intro-c0cdd0a9e4740e84.d
+++
@@ -1,5 +0,0 @@
-/Users/cmarshall/Documents/Development/Playground/rust/intro/target/debug/deps/intro-c0cdd0a9e4740e84: src/main.rs
-
-/Users/cmarshall/Documents/Development/Playground/rust/intro/target/debug/deps/intro-c0cdd0a9e4740e84.d: src/main.rs
-
-src/main.rs:
