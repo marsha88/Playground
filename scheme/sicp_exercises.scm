1ba206e0b073e82114edd4dc4c892244e9529d8b
marsha88 <marsha88@purdue.edu>
2017-07-09 11:57:08 -0400

    newtons sqrt method in scheme.

--- scheme/sicp_exercises.scm
+++ scheme/sicp_exercises.scm
@@ -34,3 +34,32 @@
 )
 
 (apply 2 3 4)
+
+#| Newton's Square Root method |#
+(define (sqrt x)
+  (if (> x 0)
+      (sqrt-iter 1.0 x)
+      -1
+  )
+)
+
+(define (sqrt-iter guess x)
+  (if (good-enough guess x)
+      guess
+      (sqrt-iter (improve guess x) x)
+  )
+)
+
+(define (avg x y)
+  (/ (+ x y) 2)
+)
+
+(define (improve guess x)
+  (avg guess (/ x guess))
+)
+
+(define (good-enough guess x)
+  (< (abs (- (square guess) x)) 0.001)
+)
+
+(sqrt 2)
