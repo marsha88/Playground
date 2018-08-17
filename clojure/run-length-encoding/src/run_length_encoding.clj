5a4e1433d2b220a3875162e88d8ded7a06185bae
marsha88 <marsha88@purdue.edu>
2018-08-17 16:23:08 -0400

    it works, but it sucks

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -13,7 +13,20 @@
               (recur (drop len plain-text) (str encoding char))
               (recur (drop len plain-text) (str encoding len char)))))))
 
+;; find better solution than this.
+(defn char-num?
+  "Returns true if given character is numeric"
+  [char]
+  (let [num (int char)]
+    (and (>= num (int \0)) (<= num (int \9)))))
+
 (defn run-length-decode
   "Decodes a run-length-encoded string"
-  [cipher-text]
-  )
+  ([cipher-text] (run-length-decode cipher-text ""))
+  ([cipher-text decoding]
+    (if (= 0 (count cipher-text)) decoding
+        (let [num (s/join (take-while char-num? cipher-text))
+              rem-cipher (drop (count num) cipher-text)]
+              (if (empty? num)
+                  (recur (drop 1 rem-cipher) (str decoding (first rem-cipher)))
+                  (recur (drop 1 rem-cipher) (str decoding (s/join (repeat (read-string num) (first rem-cipher))))))))))
